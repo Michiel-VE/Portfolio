0a83dceb7e680dac62c27b47689a43b7b9440862
Michiel-VE <r0794174@student.thomasmore.be>
2021-08-15 11:30:46 +0200

    add socials

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,33 @@
 module.exports = {
-  purge: {
-      enabled: false,
-      content: [
-          './public/*.html',
-      ],
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-        inset: {
-            '1/5': '20%',
-            '2/5': '40%',
-            '3/5': '60%',
-            '4/5': '80%',
-            '1/6': '16.67%',
-            '5/6': '83.33%',
-            '1/8': '12.5%',
-            '3/8': '37.5%',
-            '5/8': '62.5%',
-            '7/8': '87.5%',
-        },
-        backgroundImage: theme => ({
-            'home': "url('../img/bg.jpg')",
-            'social': "linear-gradient(to bottom, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.5) 75%, #000000 100%),url('../img/social.jpg')",
-        })
+    purge: {
+        enabled: false,
+        content: [
+            './public/*.html',
+        ],
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            inset: {
+                '1/5': '20%',
+                '2/5': '40%',
+                '3/5': '60%',
+                '4/5': '80%',
+                '1/6': '16.67%',
+                '5/6': '83.33%',
+                '1/8': '12.5%',
+                '3/8': '37.5%',
+                '5/8': '62.5%',
+                '7/8': '87.5%',
+            },
+            backgroundImage: theme => ({
+                'home': "url('../img/bg.jpg')",
+                'social': "linear-gradient(to bottom, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.5) 75%, #000000 100%),url('../img/social.jpg')",
+            })
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
