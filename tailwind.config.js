c91916e88ec1087ea66b7db97aba1d2a7d8fe577
Michiel_VE <r0794174@student.thomasmore.be>
2022-03-20 19:45:31 +0100

    update pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,13 +25,16 @@ module.exports = {
                 'home': "url('../img/bg.jpg')",
                 'social': "linear-gradient(to bottom, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.5) 75%, #000000 100%),url('../img/social.jpg')",
                 'php': "url('../img/php.jpg')",
-                'geo': "url('../img/qgis1.jpg')",
+                'geo': "url('../img/qgis.jpg')",
                 'lm': "url('../img/lm_sounds.jpg')",
                 'br': "url('../img/br_login.jpg')",
                 'soon': "url('../img/soon.png')",
                 'skill': "linear-gradient(180deg, rgba(255,255,255,0.4) 0%, rgba(255,255,255,0.2) 100%),url('../img/tree.jpg')",
 
             }),
+            animation: {
+                'bounce-slow': 'bounce 3s linear infinite',
+            }
         },
     },
     variants: {
